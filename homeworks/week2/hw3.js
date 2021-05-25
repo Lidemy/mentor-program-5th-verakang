8dbfe4173b829fe6e219d3964487684ca68f6282
Vera Kang <41251632+verakang@users.noreply.github.com>
2021-05-25 20:15:01 +0800

    Update hw3.js

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -4,7 +4,8 @@ function reverse(str) {
     var newWord = ''
     for (var i = str.length - 1; i >= 0; i--) {
         newWord += str[i]
-    } console.log(newWord)
+    } 
+    console.log(newWord)
 }
 
 reverse('hello');
